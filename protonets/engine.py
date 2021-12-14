c12bebbc539fa7ae9bb59092b68723f74f6d2cd8
trananhdat <36110849+trananhdat@users.noreply.github.com>
2021-12-14 14:03:37 +0700

    Update engine.py

--- protonets/engine.py
+++ protonets/engine.py
@@ -3,11 +3,14 @@ from tqdm import tqdm
 class Engine(object):
     def __init__(self):
         hook_names = ['on_start', 'on_start_epoch', 'on_sample', 'on_forward',
-                      'on_backward', 'on_end_epoch', 'on_update', 'on_end']
+                      'on_backward', 'on_end_epoch', 'on_update', 'on_end', 
+                      'loss_val', 'acc_val']
 
         self.hooks = { }
         for hook_name in hook_names:
             self.hooks[hook_name] = lambda state: None
+        self.hooks['loss_val'] = None
+        self.hooks['acc_val'] = None
 
     def train(self, **kwargs):
         state = {
@@ -19,7 +22,11 @@ class Engine(object):
             'epoch': 0, # epochs done so far
             't': 0, # samples seen so far
             'batch': 0, # samples seen in current epoch
-            'stop': False
+            'stop': False,
+            'acc_val': 0,
+            'loss_val':0,
+            'acc_train': 0,
+            'loss_train':0
         }
 
         state['optimizer'] = state['optim_method'](state['model'].parameters(), **state['optim_config'])
@@ -27,6 +34,7 @@ class Engine(object):
         self.hooks['on_start'](state)
         while state['epoch'] < state['max_epoch'] and not state['stop']:
             state['model'].train()
+            epoch_loss = []
 
             self.hooks['on_start_epoch'](state)
 
@@ -49,8 +57,11 @@ class Engine(object):
                 state['batch'] += 1
                 self.hooks['on_update'](state)
 
+                epoch_loss.append(loss.item())
+
             state['epoch'] += 1
             state['batch'] = 0
             self.hooks['on_end_epoch'](state)
 
         self.hooks['on_end'](state)
+        return state['model'].state_dict(), state['output']['acc'], sum(epoch_loss) / len(epoch_loss), state['acc_val'], state['loss_val']
